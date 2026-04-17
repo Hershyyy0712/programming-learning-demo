249601cfd3ab0db546fb7d73d13bbb58a408e778
钟振锋 <14739777077@163.com>
2026-04-17 21:31:47 +0800

    Fix formatting and spacing in shortestpath.cpp

--- Grid-Shortest-Path-BFS/shortestpath.cpp
+++ Grid-Shortest-Path-BFS/shortestpath.cpp
@@ -12,10 +12,10 @@ int shortestpath(vector<vector<int>>& grid)
 
 	grid[0][0] = 1;
 
-	q.push({0, 0, 0});
+	q.push({ 0, 0, 0 });
 	int dx[] = { 1,-1,0,0 };
-	int dy[] = { 0，0,-1,1 };
-	int nx=0， ny=0;
+	int dy[] = { 0,0,-1,1 };
+	int nx = 0, ny = 0;
 	int x, y, step;
 	while (!q.empty())
 	{
@@ -23,21 +23,21 @@ int shortestpath(vector<vector<int>>& grid)
 		y = get<1>(q.front());
 		step = get<2>(q.front());
 		q.pop();
-		
 
-		
+
+
 		for (int i = 0; i < 4; i++)
 		{
 			nx = x + dx[i];
 			ny = y + dy[i];
-			if (nx>=0&&ny>=0&&nx < n && ny < m && grid[nx][ny] != 1)
+			if (nx >= 0 && ny >= 0 && nx < n && ny < m && grid[nx][ny] != 1)
 			{
-				q.push({ nx,ny,step+1});
+				q.push({ nx,ny,step + 1 });
 				grid[nx][ny] = 1;
-			
+
 			}
-			
-			
+
+
 		}
 		if (x == n - 1 && y == m - 1)
 		{
